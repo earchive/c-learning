f543ade0830746097f047dca682c0317151c595d
EULIR <runmingli_edu@163.com>
2018-11-21 23:37:49 +0800

    update

--- src/temperature.c
+++ src/temperature.c
@@ -19,6 +19,12 @@ int temperature() {
         fahr += step;
     }
 
+    /*
+     * for (fahr = 0; fahr <= 300; fahr += 30) {
+     *      printf("%d\t%f\n", fahr, (5.0 / 9.0) * (fahr - 32));
+     * }
+     */
+
     return 0;
 }
 
