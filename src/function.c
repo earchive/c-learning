18532d59c0da0359422d9e28503a6c5f31231b47
EULIR <runmingli_edu@163.com>
2018-11-29 23:45:16 +0800

    update

--- src/function.c
+++ src/function.c
@@ -1,33 +1,46 @@
 #include <stdio.h>
 
 int max(int a, int b);
+
 void count();
+
 void change(int *a, int *b);
 
+void squeeze(char s[], int c);
+
 int fun() {
-	printf("%d\n",max(2,3));
-	for (int i = 0; i < 10; i++) {
-		count();
-	}
-	int x = 3;
-	int y = 5;
-	change(&x, &y);
-	printf("%d %d\n", x, y);
-	return 0;
+    printf("%d\n", max(2, 3));
+    for (int i = 0; i < 10; i++) {
+        count();
+    }
+    int x = 3;
+    int y = 5;
+    change(&x, &y);
+    printf("%d %d\n", x, y);
+    return 0;
 }
 
 int max(int a, int b) {
-	return a > b ? a : b;
+    return a > b ? a : b;
 }
 
 void count() {
-	//this is used to understand reserved key "static"
-	static int c = 10;
-	c++;
-	printf("%d", c);
+    //this is used to understand reserved key "static"
+    static int c = 10;
+    c++;
+    printf("%d", c);
 }
 
 void change(int *a, int *b) {
-	*a = 1;
-	*b = 2;
+    *a = 1;
+    *b = 2;
+}
+
+void squeeze(char s[], int c) {
+    int i, j;
+    for (i = j = 0; s[i] != '\0'; ++i) {
+        if (s[i] != c)
+            s[j++] = s[i];
+        s[j] = '\0';
+    }
 }
