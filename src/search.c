6cabf0a8d135f8154e63ce31a2c52c5ece7cf53b
EULIR <runmingli_edu@163.com>
2018-11-30 21:41:30 +0800

    update

---
+++ src/search.c
@@ -0,0 +1,20 @@
+//
+// Created by ANDY on 2018/11/30.
+//
+
+#include <stdio.h>
+
+int bsearch(int x, int v[], int n) {
+    int low, high, mid;
+    low = 0;
+    high = n - 1;
+    while (low <= high) {
+        mid = (low + high) / 2;
+        if (x < v[mid])
+            high = mid - 1;
+        else if (x > v[mid])
+            low = mid + 1;
+        else return mid;
+    }
+    return -1;
+}
