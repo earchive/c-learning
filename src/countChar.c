c92b27a96f5c3fa1bc0b32aef3f789de7c58b884
EULIR <runmingli_edu@163.com>
2018-12-01 19:53:17 +0800

    update

--- src/countChar.c
+++ src/countChar.c
@@ -16,5 +16,43 @@ int countChar() {
             cl++;
     }
     printf("%d\n", cl);
+    return 0;
+}
+
+int count() {
+    int c, i, nwhite, nother, ndigit[10];
+    nwhite = nother = 0;
+    for (i = 0; i < 10; ++i) {
+        ndigit[i] = 0;
+    }
+    while ((c = getchar()) != EOF) {
+        switch (c) {
+            case '0':
+            case '1':
+            case '2':
+            case '3':
+            case '4':
+            case '5':
+            case '6':
+            case '7':
+            case '8':
+            case '9':
+                ndigit[c - '0']++;
+                break;
+            case ' ':
+            case '\n':
+            case '\t':
+                nwhite++;
+                break;
+            default:
+                nother++;
+                break;
+        }
+    }
+    for (int j = 0; j < 10; ++j) {
+        printf(" %d", ndigit[j]);
+    }
+    printf("\n%d %d", nwhite, nother);
+    return 0;
 }
 
