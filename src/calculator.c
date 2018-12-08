b2027fa72e3add6266f1ac07d649491aeeb8e079
EULIR <runmingli_edu@163.com>
2018-12-08 23:40:42 +0800

    update

---
+++ src/calculator.c
@@ -0,0 +1,84 @@
+//
+// Created by ANDY on 2018/12/8.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAXOP 100
+#define NUMBER '0'
+
+int getop(char []);
+
+void push(double);
+
+double pop(void);
+
+int calculator() {
+    int type;
+    double op2;
+    char s[MAXOP];
+
+    while ((type = getop(s)) != EOF) {
+        switch (type) {
+            case NUMBER:
+                push(atof(s));
+                break;
+            case '+':
+                push(pop() + pop());
+                break;
+            case '*':
+                push(pop() + pop());
+                break;
+            case '-':
+                op2 = pop();
+                push(pop() - op2);
+                break;
+            case '/':
+                op2 = pop();
+                if (op2 != 0.0)
+                    push(pop() / op2);
+                else
+                    printf("zero divisor\n");
+                break;
+            case '\n':
+                printf("\t%.8g\n", pop());
+                break;
+            default:
+                printf("unknown command %s\n", s);
+                break;
+        }
+        return 0;
+    }
+}
+
+#define MAXVAL 100
+int sp = 0;
+double val[MAXVAL];
+
+void push(double f) {
+    if (sp < MAXVAL)
+        val[sp++] = f;
+    else
+        printf("stack full\n");
+}
+
+double pop(void) {
+    if (sp > 0)
+        return val[--sp];
+    else {
+        printf("stack empty\n");
+        return 0.0;
+    }
+}
+
+#include <ctype.h>
+
+int getch(void);
+void ungetch(int);
+
+int getop(char s[]){
+    int i,c;
+    while ((s[0] = c = getch()) == ' '||c=='\t');
+
+}
