8120a1b96e4790c92318fb898cf81bada641ed66
EULIR <runmingli_edu@163.com>
2018-12-09 23:34:19 +0800

    update

--- src/calculator.c
+++ src/calculator.c
@@ -75,10 +75,37 @@ double pop(void) {
 #include <ctype.h>
 
 int getch(void);
+
 void ungetch(int);
 
-int getop(char s[]){
-    int i,c;
-    while ((s[0] = c = getch()) == ' '||c=='\t');
+int getop(char s[]) {
+    int i, c;
+    while ((s[0] = c = getch()) == ' ' || c == '\t');
+    s[1] = '\0';
+    if (!isdigit(c) && c != '.')
+        return c;
+    i = 0;
+    if (isdigit(c))
+        while (isdigit(s[++i] = c = getch()));
+    if (c == '.')
+        while (isdigit(s[++i] = c = getch()));
+    s[i] = '\0';
+    if (c != EOF)
+        ungetch(c);
+    return NUMBER;
+}
 
+#define BUFSIZE 100
+
+char buf[BUFSIZE];
+int bufp = 0;
+
+int getch(void) {
+    return (bufp > 0) ? buf[--bufp] : getchar();
+}
+
+void ungetch(int c) {
+    if (bufp >= BUFSIZE)
+        printf("characters overload\n");
+    else buf[bufp++] = (char) c;
 }
