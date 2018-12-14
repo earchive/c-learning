55335f24c0c888a25d8845aa333d18092c1e03f2
EULIR <runmingli_edu@163.com>
2018-12-14 23:29:53 +0800

    update

---
+++ src/macro.c
@@ -0,0 +1,13 @@
+//
+// Created by ANDY on 2018/12/14.
+//
+
+#define MAX 1000
+#define forever for (;;)
+#define max(A, B) ((A) > (B) ? (A) : (B))
+#define dprint(expr) printf(#expr " = %g\n", expr)
+#define main mian
+#define true false
+#define flase false
+
+
