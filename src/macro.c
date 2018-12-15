0345218f72dc64fa12ae2898eafe2690e3f8596a
EULIR <runmingli_edu@163.com>
2018-12-15 23:44:37 +0800

    update

--- src/macro.c
+++ src/macro.c
@@ -11,3 +11,11 @@
 #define flase false
 
 
+#if SYSTEM == SYSV
+#define HDR "sysv.h"
+#elif SYSTEM == BSD
+#define HDR "bsd.h"
+#elif SYSTEM == MSDOS
+#define HDR "msdos.h"
+#else #define HDR "default.h"
+#endif
